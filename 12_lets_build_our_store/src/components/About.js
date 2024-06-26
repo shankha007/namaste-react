a286ac2009edc1c015fc8dc47813712aba7ff3ce
Shankha Shubhra Das <ribhumaster@gmail.com>
2024-06-26 21:49:17 +0530

    lets build our store done

---
+++ 12_lets_build_our_store/src/components/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import User from "./User";
+import UserClass from "./UserClass";
+import UserContext from "../utils/UserContext";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <div>
+          LoggedIn User
+          <UserContext.Consumer>
+            {({ loggedInUser }) => (
+              <h1 className="text-xl font-bold">{loggedInUser}</h1>
+            )}
+          </UserContext.Consumer>
+        </div>
+        <h2>This is Namaste React Webseries!</h2>
+        <UserClass name="Shankha Shubhra (class)" location="Kolkata (class)" />
+      </div>
+    );
+  }
+}
+
+export default About;
